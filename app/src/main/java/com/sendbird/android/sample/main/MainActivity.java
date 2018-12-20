dd8e77e3b8964ce39db489ecb51a933e507f6f97
Rommel Sunga <rommel.sunga@sendbird.com>
2018-12-20 15:24:04 -0800

    Minor change

--- app/src/main/java/com/sendbird/android/sample/main/MainActivity.java
+++ app/src/main/java/com/sendbird/android/sample/main/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
 
+    // Minor change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
